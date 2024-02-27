3ed7d5d042c9cc59f142d1645ac99787c8d4e520
tunist <mrmpjackson@gmail.com>
2024-02-27 12:42:38 +0000

    update to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,24 @@
 # activate the venv
 source venv/bin/activate;
 
+
+
+# Check if .env file exists
+if [ ! -f "./DataFlowDirectory/.env" ]; then
+    echo "Creating .env file..."
+
+    # Generate a Django secret key
+    SECRET_KEY=$(python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())')
+
+    # Create the .env file and write the secret key
+    echo "SECRET_KEY=${SECRET_KEY}" > .env
+
+    echo ".env file created."
+else
+    echo ".env file already exists."
+fi
+
+
 # navigate to the project's root directory
 cd DataFlowDirectory/DataFlow;
 
